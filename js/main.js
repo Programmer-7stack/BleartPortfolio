222dfc44436328ae8dff93faa2b4e4abceef2cf6
Programmer <bleartsejdiu4@gmail.com>
2025-02-28 15:44:16 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -121,7 +121,4 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 
-  window.onload = function() {
-        // Reset the form fields when the page loads
-        document.getElementById("form").reset();
-    };
+ 
